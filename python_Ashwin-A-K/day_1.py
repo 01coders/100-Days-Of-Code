70911cb24954b32dd6c5bd2302331003d0c6e31b
Ashwin-A-K <ashwinkumar3098@gmail.com>
2019-05-01 22:41:18 -0700

    arth_ops

---
+++ python_Ashwin-A-K/day_1.py
@@ -0,0 +1,18 @@
+# this is the first comment
+import sys
+
+s1 = sys.argv[1]  # arugument passing in python
+print(s1)  # if args no passed, then IndexError: list index out of range is occured
+
+print(1+1)  # the operators +, -, *, / and () work similarly
+
+print(6/3)  # division returns floating number
+print(5/3)
+print(17//3)  # floor division doesn't consider decimal places
+print(7.0//3)  # Here decimal place is displayed even if floor division is done
+
+print(5 % 2)  # modulus is used return remainder of the division
+
+print(5**3)  # ** is used to perform power operation
+
+# print(1 + _) (works only on ipython) the last printed expression is assigned to the variable _
