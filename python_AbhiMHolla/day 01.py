171cb0d4790fd93cfb569b7a74b777de0cdcdd4b
unknown <abhishek.holla@outlook.com>
2019-05-04 09:53:47 +0530

    Print_function

---
+++ python_AbhiMHolla/day 01.py
@@ -0,0 +1,20 @@
+#print difference 
+c = 20 #initializing the value  of a variable
+print("the value of a string" +str(c)) #printing c value as string
+print("the value of a string %d" %(c))
+
+#accepting value from users
+a=input("enter  a value ")
+b=input("enter b value")
+
+#printing value as string
+print(a +" "+ b ) #printing value of a and b as string with space
+print(a + b) #printing value of a and b as string without space
+
+#type conversion
+a=int(input("enter  a value "))
+b=int(input("enter b value"))
+
+print (a+b) #printing value as int
+
+print(a, end=" ") #append space at the end
