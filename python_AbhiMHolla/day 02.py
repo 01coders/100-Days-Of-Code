76eeda78f2effa39b3cd6775c823fe0e47a3b3fc
unknown <abhishek.holla@outlook.com>
2019-05-06 21:19:42 +0530

    Arithmetic_operations_part_01

---
+++ python_AbhiMHolla/day 02.py
@@ -0,0 +1,28 @@
+# Arithmetic operations
+
+#basic Arithmetic operators part 1
+
+x = 5
+y = 3
+z=15
+print(x + y)
+print(z-y)
+print(x*y)
+print(z/y)
+print(z%x)
+
+#comparison operators
+    
+a = 15
+b = 23
+print(a == b)  # returns False because 15 is not equal to 23
+print(a != b)  # returns True because 15 is not equal to 23
+print(a>b)     # returns False because 15 is not greater than 23
+print(a<b)     # returns True because 15 is less than to 23
+print(a>=b)    # returns False because 15 is not greater than 23
+print(a<=b)    # returns True because 15 is less than to 23
+
+
+
+
+
