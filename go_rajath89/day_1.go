e86c199a2bea5e43e5eb50f79fceabfa314d08ad
Naga Rajath S M <nagarajathsm@gmail.com>
2019-05-07 19:14:45 +0530

    initial commit

---
+++ go_rajath89/day_1.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("day1")
+
+	a, b := 2, 7
+
+	c := a + b
+	println(c)
+
+	var float = 2.3
+	fmt.Printf("%T\n", float) //default val float64
+
+}
