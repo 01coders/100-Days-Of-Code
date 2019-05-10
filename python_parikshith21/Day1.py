013202f81126f45dd23263a192248086731569a0
Parikshith21 <parikshithh@gmail.com>
2019-05-10 13:27:35 +0530

    Print & variables

---
+++ python_parikshith21/Day1.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 10 11:46:17 2019
+
+@author: Parikshith.H
+"""
+
+#************Print in Python*****************#
+
+print("Hi")  #Hi
+print('Hi Hello')  #Hi Hello
+
+a=10
+print('a')  #2
+print(a)    #10
+print("The value is " ,a)  #The value is 10
+print("The value is",a,".")   #The value is 10 .
+
+s="Hi"
+print("Welcome",s)   #Welcome Hi
+print("welcome"+s)   #welcomeHi
+
+print("The value \n is " ,a)  #The value
+                              #is 10
+                              
+print("The value \t is ", a)  #The value        is  10
+print("The value"+"\n"+"is",a)   #The value
+                                 #is 10
+                                 
+                                
+#*************VAriables in Python**************#
+a=10
+b=3
+c=a/b
+
+print(c)   #3.3333333333333335
+print(int(c))  #3
+
+d=a//b     #truncate_division
+print(d)   #3
+
+s = 'hi'   
+print(s)   #hi
+                                 
+print(type(s)) #type is returned it is taken as object  #<class 'str'>
+print(type(a))  #<class 'int'>
+
+f=2**3 #exponent operation if ** => 2^3
+print(f)   #8
+                                 
