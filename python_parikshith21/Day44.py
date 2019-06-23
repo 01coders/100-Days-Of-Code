f093314d38511343941e9db131e86926ebbfa765
unknown <parikshithh@gmail.com>
2019-06-23 19:49:08 +0530

    numpy_array

---
+++ python_parikshith21/Day44.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 23 19:32:48 2019
+
+@author: Parikshith.H
+"""
+
+import numpy as np 
+  
+a = np.array([1, 2, 5, 3]) 
+  
+# add 1 to every element 
+print ("Adding 1 to every element:", a+1) 
+  
+# subtract 3 from each element 
+print ("Subtracting 3 from each element:", a-3) 
+  
+# multiply each element by 10 
+print ("Multiplying each element by 10:", a*10) 
+  
+# =============================================================================
+# #output:
+# Adding 1 to every element: [2 3 6 4]
+# Subtracting 3 from each element: [-2 -1  2  0]
+# Multiplying each element by 10: [10 20 50 30]
+# =============================================================================
