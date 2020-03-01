ff97f091e6fbcedb99ded2c2f329a9aba77942a0
zshzero <ashwinkumar3098@gmail.com>
2020-03-01 18:04:07 +0530

    Handling Args and Shell Built-ins

--- rust_Ashwin-A-K/fridaysh/src/main.rs
+++ rust_Ashwin-A-K/fridaysh/src/main.rs
@@ -5,7 +5,9 @@
 // The characters the shell outputs to stdout and stderr get sent to the terminal, which in turn draws these characters in the window.
 
 fn main() {
+    use std::env;
     use std::io::{stdin, stdout, Write};
+    use std::path::Path;
     use std::process::Command; // https://doc.rust-lang.org/std/process/struct.Command.html
 
     loop {
@@ -15,12 +17,32 @@ fn main() {
         let mut input = String::new(); // Declaring a variable to store user input
         stdin().read_line(&mut input).unwrap(); // reading input from user
 
-        let command = input.trim(); // read_line leaves a trailing newline, which trim removes
+        let mut parts = input.trim().split_whitespace(); // everything after the first whitespace character is interpreted as args
+                                                         // print!("{:#?}", parts.next());
+        let command = parts.next().unwrap(); // https://doc.rust-lang.org/std/iter/trait.Iterator.html#examples
+                                             // next() advances the iterator present in "parts" variable and returns the next value
+        let args = parts; // It has arguments that are seperated by whitespace
 
-        let mut child = Command::new(command) // program gives a path to the program to be executed
-            .spawn() // Executes the command as a child process, waiting for it to finish and collecting all of its output
-            .unwrap(); // It is should to handle errors
+        match command {
+            "cd" => {
+                let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                // peekable() creates a iterator which can use peek to look at the next element of the iterator without consuming it
+                // peek() returns a reference value without advancing the iterator
+                // default to '/' as new directory if one was not provided
+                // map_or() applies a function to the contained value (if any), or returns the provided default (if not)
+                let root = Path::new(new_dir);
+                if let Err(e) = env::set_current_dir(&root) {
+                    eprintln!("{}", e);
+                }
+            }
+            command => {
+                let mut child = Command::new(command) // program gives a path to the program to be executed
+                    .args(args)
+                    .spawn() // Executes the command as a child process, waiting for it to finish and collecting all of its output
+                    .unwrap(); // It is should to handle errors
 
-        child.wait().expect("failed to wait on child"); // don't accept another command until this one completes
+                child.wait().expect("failed to wait on child"); // don't accept another command until this one completes
+            }
+        }
     }
 }
