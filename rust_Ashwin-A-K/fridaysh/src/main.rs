98928cf0e186ee295bc90a4da0aa9554e2918659
zshzero <ashwinkumar3098@gmail.com>
2020-03-04 18:07:12 +0530

    Completed implementing cd

--- rust_Ashwin-A-K/fridaysh/src/main.rs
+++ rust_Ashwin-A-K/fridaysh/src/main.rs
@@ -1,48 +1,67 @@
-// The terminal emulator is just the window. It runs a text based program, which by default is your login shell (Ex: bash).
-
-// When you type characters in the window, the terminal draws these characters in the window in addition to sending it to the shell’s stdin.
-
-// The characters the shell outputs to stdout and stderr get sent to the terminal, which in turn draws these characters in the window.
+use std::env;
+use std::io::{stdin, stdout, Write};
+use std::path::Path;
+use std::process::{Child, Command, Stdio};
 
 fn main() {
-    use std::env;
-    use std::io::{stdin, stdout, Write};
-    use std::path::Path;
-    use std::process::Command; // https://doc.rust-lang.org/std/process/struct.Command.html
-
     loop {
-        print!("fsh> "); // use the `>` character as the prompt
-        stdout().flush(); // need to explicitly flush. this to ensure it prints before read_line
+        print!("> ");
+        stdout().flush();
 
-        let mut input = String::new(); // Declaring a variable to store user input
-        stdin().read_line(&mut input).unwrap(); // reading input from user
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
 
-        let mut parts = input.trim().split_whitespace(); // everything after the first whitespace character is interpreted as args
-                                                         // print!("{:#?}", parts.next());
-        let command = parts.next().unwrap(); // https://doc.rust-lang.org/std/iter/trait.Iterator.html#examples
-                                             // next() advances the iterator present in "parts" variable and returns the next value
-        let args = parts; // It has arguments that are seperated by whitespace
+        let mut commands = input.trim().split(" | ").peekable();
+        let mut previous_command = None;
 
-        match command {
-            "cd" => {
-                let new_dir = args.peekable().peek().map_or("/", |x| *x);
-                // peekable() creates a iterator which can use peek to look at the next element of the iterator without consuming it
-                // peek() returns a reference value without advancing the iterator
-                // default to '/' as new directory if one was not provided
-                // map_or() applies a function to the contained value (if any), or returns the provided default (if not)
-                let root = Path::new(new_dir);
-                if let Err(e) = env::set_current_dir(&root) {
-                    eprintln!("{}", e);
+        while let Some(command) = commands.next() {
+            let mut parts = command.trim().split_whitespace();
+            let command = parts.next().unwrap();
+            let args = parts;
+
+            match command {
+                "cd" => {
+                    let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                    let root = Path::new(new_dir);
+                    if let Err(e) = env::set_current_dir(&root) {
+                        eprintln!("{}", e);
+                    }
+
+                    previous_command = None;
+                }
+                "exit" => return,
+                command => {
+                    let stdin = previous_command.map_or(Stdio::inherit(), |output: Child| {
+                        Stdio::from(output.stdout.unwrap())
+                    });
+
+                    let stdout = if commands.peek().is_some() {
+                        Stdio::piped()
+                    } else {
+                        Stdio::inherit()
+                    };
+
+                    let output = Command::new(command)
+                        .args(args)
+                        .stdin(stdin)
+                        .stdout(stdout)
+                        .spawn();
+
+                    match output {
+                        Ok(output) => {
+                            previous_command = Some(output);
+                        }
+                        Err(e) => {
+                            previous_command = None;
+                            eprintln!("{}", e);
+                        }
+                    };
                 }
             }
-            command => {
-                let mut child = Command::new(command) // program gives a path to the program to be executed
-                    .args(args)
-                    .spawn() // Executes the command as a child process, waiting for it to finish and collecting all of its output
-                    .unwrap(); // It is should to handle errors
+        }
 
-                child.wait().expect("failed to wait on child"); // don't accept another command until this one completes
-            }
+        if let Some(mut final_command) = previous_command {
+            final_command.wait();
         }
     }
 }
